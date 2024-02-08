4ef9073b66d23fbda48f0ef7ed7ab8e0d7e2e213
optimuspab <122415833+optimuspab@users.noreply.github.com>
2024-02-08 16:32:12 -0300

    fix multiples videos

--- ffmpeg-multiples-videos/ffmpeg-video-converter.py
+++ ffmpeg-multiples-videos/ffmpeg-video-converter.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 import ffmpeg
 import threading
 
@@ -15,20 +16,18 @@ def procesar_archivos(files):
     for input_file in files:
         output_file = input_file.split('.')[0] + '_convertido.mp4'
         convertir_video(input_file, output_file)
-    tk.messagebox.showinfo("Convertido", "Todos los videos han sido convertidos con éxito.")
+    messagebox.showinfo("Convertido", "Todos los videos han sido convertidos con éxito.")
 
 def seleccionar_archivos():
     input_files = filedialog.askopenfilenames()
     if input_files:
-        # Convertir la tupla de archivos seleccionados a una lista
         files_to_process = list(input_files)
-        # Iniciar un hilo separado para procesar los archivos encolados
         threading.Thread(target=procesar_archivos, args=(files_to_process,)).start()
 
 app = tk.Tk()
 app.title("Convertidor de Video")
 
-btn_seleccionar = tk.Button(app, text="Seleccionar Video", command=seleccionar_archivo)
+btn_seleccionar = tk.Button(app, text="Seleccionar Video", command=seleccionar_archivos)
 btn_seleccionar.pack(pady=20)
 
 app.mainloop()
